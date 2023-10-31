40193f0f3fa30acdc8a286f2f01c4c21c356e880
Douglas Sá <81888493+XDougSa@users.noreply.github.com>
2023-10-31 17:23:14 -0300

    Update index.php

--- EngSof/index.php
+++ EngSof/index.php
@@ -3,6 +3,9 @@
 $url = $_SERVER["REQUEST_URI"];
 
 switch($url){
+    case "/":
+        include("src/view/cadastro.php");
+        break;
     case "/cadastro": 
         include("src/view/cadastro.php");
         break;
